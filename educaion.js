45e287301257bec3fe17db6affba6a1530d7fcf1
k3darshuzz <68709284+k3darshuzz@users.noreply.github.com>
2020-07-23 21:53:35 +0200

    Add files via upload
    My first project

---
+++ educaion.js
@@ -0,0 +1,7 @@
+function logo(){
+	arlet('View Educaional progress of Tinashe');
+	document.write('Went to Tadzembwa Primary School\n'+'\n Went to Shingai Primary School Chiredzi\n'+
+	'\n Went to Chirowakamwe Primary School\n'+'\n Went to Zimuto High School\n'+'\n Went to Mutendi High School \n'+
+	'\n Currently Studying at HIT \n');
+	
+}
